ca4af9e5ba38a93a31352440afdd0ce27f607d81
Mostak-Mridul <mmridul116@gmail.com>
2025-05-04 02:29:13 +0600

    verify token changes

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,36 +1,39 @@
 import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 
-export const verifyToken = (req, res, next) =>{
-    const token = req.cookies["access-token"];
-    if(!token){
-        return next(createError(401,"You are not authorized"))
+export const verifyToken = (req, res, nextCallback) => {
+  const token = req.cookies["access-token"];
+  if (!token) {
+    return nextCallback(createError(401, "You are not authorized"));
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return nextCallback(createError(403, "Token is not valid"));
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) =>{
-        if(err){
-            return next(createError(403,"Token is not valid"))
-        }
-        req.user = user;
-        next();
-    })
-}
+    req.user = user;
+    nextCallback();
+  });
+};
 
-export const verifyUser = (req, res, next) =>{
-    verifyToken(req, res,next, ()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-            next();
-        }else{
-            return next(createError(405, "You are not authorized to perform this action"))
-        }
-    })
-}
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, (err) => {
+    if (err) return next(err);
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(405, "You are not authorized"));
+    }
+  });
+};
 
-export const verifyAdmin = (req, res, next) =>{
-    verifyToken(req, res,next, ()=>{
-        if(req.user.isAdmin){
-            next();
-        }else{
-            return next(createError(405, "You are not authorized to perform this action"))
-        }
-    })
-}
+export const verifyAdmin = (req, res, next) => {
+  verifyToken(req, res, (err) => {
+    if (err) return next(err);
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(405, "You are not authorized"));
+    }
+  });
+};
